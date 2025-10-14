60f07d79cc45ca20eef714cc252323136d2a3f5f
chaemina <chaemina82@naver.com>
2025-10-15 02:52:14 +0900

    fix: 부자연스러운 위치 이동 해결

--- my-app/shared/components/atoms/Banner.tsx
+++ my-app/shared/components/atoms/Banner.tsx
@@ -3,11 +3,11 @@ export default function Banner() {
     <div className="flex flex-col w-full gap-5">
       <img 
         src={'/images/CONNECT.svg'} 
-        className="block  w-screen" 
+        className="w-screen" 
       />
       <img 
         src={'/images/EVERYONE.svg'} 
-        className="block md:hidden w-full" 
+        className="md:hidden w-screen" 
       />
     </div>
   );
--- my-app/shared/components/templates/MainTemplate.tsx
+++ my-app/shared/components/templates/MainTemplate.tsx
@@ -1,19 +1,21 @@
-"use client"
+"use client";
 
 import Banner from "../atoms/Banner";
 import useHoverOpacity from "../../hooks/useHoverOpacity";
 import { aboutme } from "../../constants/aboutme";
 import { useRouter } from "next/navigation";
-import profile from "../../../public/images/Profile.jpg"
 
 export default function MainTemplate() {
   const router = useRouter();
-  const { opacity, onEnter, onLeave, onClick } = useHoverOpacity({ rest: 0, hover: 1 });
+  const { opacity, onEnter, onLeave, onClick } = useHoverOpacity({
+    rest: 0,
+    hover: 1,
+  });
 
   return (
     <main className="h-full w-full flex flex-col justify-between">
       <div className="grid flex-1 text-xl p-2 gap-4 grid-cols-1 sm:grid-cols-2 auto-rows-auto">
-        {/* left */}
+
         <div className="grid">
           <div className="flex flex-col">
             <span>CHAEMINA</span>
@@ -22,25 +24,26 @@ export default function MainTemplate() {
             <span>Web/Application</span>
           </div>
 
-          <div className="" style={{opacity}}>
-            <img src="/images/Profile.jpg" width={200} className="2xl:w-90"/>
-
+          <div className="duration-300" style={{ opacity }}>
+            <img src="/images/Profile.jpg" width={200} className="2xl:w-90" />
           </div>
-        
         </div>
 
-        <div className="grid">
-          <div
-            className="lg:w-3/4 hidden md:block transition-opacity duration-300"
-            style={{ opacity }}
-          >
-            {aboutme}
+
+        <div className="grid relative">
+          <div className="relative w-3/4 h-[200px] hidden md:block">
+            <div
+              className="absolute top-0 left-0 w-full h-full transition-opacity duration-300 md:text-base lg:text-xl"
+              style={{ opacity }}
+            >
+              {aboutme}
+            </div>
           </div>
 
-          <div className="flex flex-col lg:justify-start">
+          <div className="flex flex-col justify-start">
             <a href="mailto:chaemina82@naver.com">Email</a>
-            <a onClick={() => router.push("/archive")}> Project Archive </a>
-            <a onClick={() => router.push("/bio")}> Biography </a>
+            <a onClick={() => router.push("/archive")}>Project Archive</a>
+            <a onClick={() => router.push("/bio")}>Biography</a>
             <a href="https://github.com/chaemina">Github</a>
             <a
               onMouseEnter={onEnter}
@@ -56,12 +59,9 @@ export default function MainTemplate() {
 
       <Banner />
 
-      <div
-            className="md:hidden transition-opacity duration-300"
-            style={{ opacity }}
-          >
-            {aboutme}
-          </div>
+      <div className="md:hidden transition-opacity duration-300" style={{ opacity }}>
+        {aboutme}
+      </div>
     </main>
   );
 }
